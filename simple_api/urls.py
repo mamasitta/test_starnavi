3ea91c94ac904393716a77a604752ee3e916b027
Victoria <vikiber2013@gmail.com>
2021-01-14 14:08:42 +0200

    remove test view

--- simple_api/urls.py
+++ simple_api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+
     path('api/user_signup/', views.user_signup, name='register_user'),
     path('api/login/', views.login_view, name='login'),
     path('api/create_post/', views.create_post, name='create_post'),
